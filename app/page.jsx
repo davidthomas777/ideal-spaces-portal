5d13e0dea22d555735e8cfb33a7fbb7bea79f105
David Thomas <thomasdw@uci.edu>
2025-01-07 23:20:41 -0800

    Changed typescript to javascript

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-left">
+          Ideal Spaces
+        <br className="max-md: hidden" />
+      </h1>
+      <p>
+        Promptopia is an open-source AI prompting tool 
+        for the modern world to discover, create, 
+        and share creative prompts.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
