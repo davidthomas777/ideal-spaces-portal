f1566cb9e9263fc4d549087858b98f23989699a9
David Thomas <thomasdw@uci.edu>
2025-01-08 19:48:31 -0800

    Added components

---
+++ app/components/loginbutton.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react';
+
+const LoginButton = () => {
+    const handleLogin = () => {
+        window.location.href = '/login'; // Redirect to the login page
+    };
+
+    return (
+        <button onClick={handleLogin} className="px-4 py-2 bg-red-500">
+            Login
+        </button>
+    );
+};
+
+export default LoginButton
--- app/components/toolbar.jsx
+++ app/components/toolbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from 'react'
+import LoginButton from "./loginbutton"
+
 
 const Toolbar = () => {
     return (
         <div className="flex justify-center w-full px-4 py-3 fixed top-0">
             <p className='text-white'>test</p>
+            <LoginButton/>
         </div>
     )
 }
