cd51a5fa1faf32527e6a271fae6fd1d10844a899
David Thomas <thomasdw@uci.edu>
2025-01-08 19:49:09 -0800

    Fixing main home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from "react"
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
 import Toolbar from "./components/toolbar"
 
-const Home = () => {
+async function Home() {
+  const session = await getServerSession(options)
+
   return (
     <div className="w-full flex-center flex-col">
       <Toolbar/>
